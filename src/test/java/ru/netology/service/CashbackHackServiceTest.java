53ff424294e2dbedca86811b60746afcfb6d7a5c
Kulichkin <kulichkin@savinger.ru>
2023-07-12 13:19:44 +0300

    TestNg code added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.service;
+
+
+import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.*;
+
+
+public class CashbackHackServiceTest {
+    CashbackHackService cashback = new CashbackHackService();
+
+    @Test
+    public void shouldCalculateCashBackEqual1000() {
+        int amount = 1000;
+
+        int expected = 1000;
+        int actual = cashback.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateCashBackAbove1000() {
+        int amount = 1200;
+
+        int expected = 800;
+        int actual = cashback.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateCashBackBelow1000() {
+        int amount = 800;
+
+        int expected = 200;
+        int actual = cashback.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+}
